4628cb7942939c5a32c9ab48d1b3867c69f65109
Marc Capdevila <cramca49@gmail.com>
2018-03-09 19:33:35 +0100

    login test

---
+++ helloWorldXamarin/helloWorldXamarin/helloWorldXamarinPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace helloWorldXamarin
+{
+    public partial class helloWorldXamarinPage : ContentPage
+    {
+        public helloWorldXamarinPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked (object sender, EventArgs args)
+        {
+            string userName = nameEntry.Text;
+            string userPassword = passwordEntry.Text;
+            DisplayAlert("Hello", $"Hello {userName} your password is {userPassword}", "Cancel");
+        }
+    }
+}
